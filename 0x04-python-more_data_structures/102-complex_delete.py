b49900899ef80cc9901446bb2323393b938cb687
GKitavi <gearadkitavi@gmail.com>
2023-06-14 23:41:14 +0000

    102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    """
-     A function that deletes keys with a specific value in a dictionary.
-    """
-    for key in list(a_dictionary.keys()):
-        if a_dictionary[key] == value:
-            del a_dictionary[key]
-    return a_dictionary
+    if(value):
+        for key, val in list(a_dictionary.items()):
+            if val is value:
+                del a_dictionary[key]
+        return(a_dictionary)
